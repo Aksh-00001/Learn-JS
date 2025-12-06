c9fac12bc0a585ea70499112189627b6e0d764bb
CLIMBER <138255495+Aksh-00001@users.noreply.github.com>
2025-12-06 20:13:09 +0000

    data types

---
+++ Basics/dataTypes.js
@@ -0,0 +1,24 @@
+"use strict"; // this treats all JS codes as newer version of JS
+
+// alert ("hello")
+// alert (3+3) // we are using nodejs not browser 
+ 
+// Data types 
+let name = "aksh"// string 
+let age = 21  // number
+let isLoggedIn = false // boolean
+
+// number => can store 2 to Power 53  
+// bigint => stores very very large number 
+// string ="" 
+// boolean => true/false
+// null => standalone value
+// undefined => no value assigned
+// symbol => unique
+
+// object
+
+console.log (typeof age ); // gives the data type . output=> number
+console.log(typeof undefined) // Output=> undefined
+console.log (typeof null) // output=> object // null is an object 
+
