fd1adee13b5ec4611bebace60563e16606be5ae2
CLIMBER <138255495+Aksh-00001@users.noreply.github.com>
2025-12-09 18:44:08 +0000

    data type summary

---
+++ dataTypesSummary.js
@@ -0,0 +1,20 @@
+// primitive datatypes 
+
+// 7 types : string , number , boolean , null , undefined , symbol , bigint
+
+
+
+// referance /non-primitive type
+
+// array, object , functions
+
+const heros = ["Krish", "spiderman"]//aray
+//objects
+let myobj= { 
+    name: "aksh",
+    age: 22,
+}
+// function
+const myfunction= function(){
+    console.log("hello world")
+}
