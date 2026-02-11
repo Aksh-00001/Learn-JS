45bc72dda8ea8d4fde64dd446193237fac529977
JUSTICE <138255495+Aksh-00001@users.noreply.github.com>
2026-02-11 16:25:20 +0000

    object part 3 done

---
+++ objects3.js
@@ -0,0 +1,13 @@
+// destructuring of objects in javascript 
+
+const course ={
+    coursename: "javascript with me",
+    price: "999",
+    couseInstructor: "aksh"
+}
+
+const {couseInstructor: instructor} = course
+//console.log(couseInstructor)
+console.log(instructor)
+
+
