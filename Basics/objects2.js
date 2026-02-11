e9d102034bacd8762d92c4aaf63d541c8a3e0ba8
JUSTICE <138255495+Aksh-00001@users.noreply.github.com>
2026-02-11 16:03:40 +0000

    part 2 done

--- Basics/objects2.js
+++ Basics/objects2.js
@@ -19,3 +19,34 @@ const tinderUser = {}
  console.log(regularUser.fullanme.userFullname)
  console.log(regularUser.fullanme.userFullname.lastname)
  console.log(regularUser.fullanme?.userFullname.lastname) // the ? acts as if else
+
+
+const obj1 = {1:"a", 2:"b"}
+const obj2 = {3:"a", 4:"b"}
+
+const obj3 = Object.assign({}, obj1, obj2) // to merge two objects ----> target and source
+console.log(obj3)
+
+const obj4 = {...obj1 , ...obj2} // ... is the spread operator 
+console.log(obj4)
+
+const users =[{
+    id: 1,
+    email: "a@gmail.com"
+},
+{
+    id: 2,
+    email: "a@gmail.com"
+
+},
+
+{
+    id: 3,
+    email: "a@gmail.com"
+},]
+
+users[1].email
+console.log(tinderUser)
+console.log(Object.keys(tinderUser))
+console.log(Object.values(tinderUser))
+console.log(Object.entries(tinderUser))
